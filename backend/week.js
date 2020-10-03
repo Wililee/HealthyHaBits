0891e8b1812337137c5b6307885740aa683fd0f1
Connor <morric27@mcmaster.ca>
2020-10-03 14:50:22 -0400

    Update week.js

--- backend/week.js
+++ backend/week.js
@@ -1,8 +1,17 @@
 class week {
+
     constructor () {
+        var daysOfWeek = [];
+        var weekdayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+        var i;
+        for (i = 0; i < 7; i++) {
+            daysOfWeek.push(new day(weekdayNames[i]));
+        }
     }
 
-    getWeekDays() {
-        return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    getWeekDay(num) {
+        weekdayNum = num % 7; // use mod to stop index error
+        return daysOfWeek[num];
     }
 }
