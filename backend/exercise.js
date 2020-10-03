942f97d43130dc66492ea5cfa771039811eb4590
Jeffrey Wang <jwjeff200@gmail.com>
2020-10-03 15:05:43 -0400

    Create exercise.js

---
+++ backend/exercise.js
@@ -0,0 +1,20 @@
+class exercise extends time_slot {
+
+    constructor(day_of_week, slot_num, type, calories_burned, workout) {
+        super(day_of_week, slot_num, type);
+        // Integer value indicating how many calories were burned
+        this.calories_burned = calories_burned;
+        // String value indicating what workout was done
+        this.workout = workout;
+    }
+
+    // Returns the number of calories burned
+    getCalories(){
+        return this.calories_burned;
+    }
+    // Returns the type of workout completed
+    workout(){
+        return this.workout;
+    }
+
+}
