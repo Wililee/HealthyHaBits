72ec63a6b582d16b5dd91dfba84a38c102f62101
Jeffrey Wang <jwjeff200@gmail.com>
2020-10-03 23:27:17 -0400

    stylized buttons

--- frontend/web_app/assets/js/main.js
+++ frontend/web_app/assets/js/main.js
@@ -1,3 +1,11 @@
+window.addEventListener("load", function(){
+    document.querySelector(".preloader").classList.add("opacity-0");
+
+    setTimeout(function(){
+        document.querySelector(".preloader").style.display="none";
+    } ,1000)
+})
+
 //  Navigation Controls
 const nav = document.querySelector(".nav"),
       navList = nav.querySelectorAll("li"),
