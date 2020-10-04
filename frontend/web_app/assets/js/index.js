aaa5a67095ca8f362a96b2e8e90eeeae03e3a6ba
jsrebrnjak <j.srebrnjak@hotmail.com>
2020-10-04 11:42:06 -0400

    values

--- frontend/web_app/assets/js/index.js
+++ frontend/web_app/assets/js/index.js
@@ -15,7 +15,45 @@ function toggleForm() {
 }
 
 var UserName;
+var Age;
+var Gender;
+var Height;
+var Weight;
 $('#SubmitBtn').on('click', () => {
-    UserName = $('#nameTxt').val();
-    
+    UserName = $('#nameTxti').val();
+    Age = $('#agei').val();
+    Gender = $('#genderi').val();
+    Height = $('#heighti').val();
+    Weight = $('#weighti').val();
+})
+
+var monT1;
+var monT2;
+var tuesT1;
+var tuesT2;
+var wedT1;
+var wedT2;
+var thursT1;
+var thursT2;
+var friT1;
+var friT2;
+var satT1;
+var satT2;
+var sunT1;
+var sunT2;
+$('#SubmitBtn2').on('click', () => {
+    monT1 = $('#monStarti').val();
+    monT2 = $('#monEndi').val();
+    tuesT1 = $('#tuesStarti').val();
+    tuesT2 = $('#tuesEndi').val();
+    wedT1 = $('#wedStarti').val();
+    wedT2 = $('#wedEndi').val();
+    thursT1 = $('#thursStarti').val();
+    thursT2 = $('#thursEndi').val();
+    friT1 = $('#friStarti').val();
+    friT2 = $('#friEndi').val();
+    satT1 = $('#satStarti').val();
+    satT2 = $('#satEndi').val();
+    sunT1 = $('#sunStarti').val();
+    sunT2 = $('#sunEndi').val();
 })
