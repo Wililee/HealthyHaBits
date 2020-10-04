651435da42568988174e2d8e762f6d6678b22335
Will Lee <47044936+Wililee@users.noreply.github.com>
2020-10-03 22:21:12 -0400

    Update Schedule.js

--- backend/Schedule.js
+++ backend/Schedule.js
@@ -3,7 +3,7 @@ class Schedule{
     //user data is the list of timeslots in which the user is working
     constructor(userData){
         //creates a week
-        this.w = new week;
+        this.w = new Week;
         userData.forEach(slot => {
             var dow = slot.day_of_week;
             var sn = slot.slot_num;
@@ -16,53 +16,54 @@ class Schedule{
         var RecommendedSleepNum = 18;
         this.w.daysOfWeek.forEach(d =>{
             var flag = false;
-
+            var lock = false;
             //first trys to find a base
             //only do this if the base isnt already set
-            if (d.base !== null)
+            
             d.time_slots.forEach(s =>{
                 if (s.slot_num === 0 && s.type === 'W'){
                     flag = true;
-                }
-                
+                }   
                 //this would be the first blank after so just set flag to true
-                if (s.type === null){
+                else if (s.type === null && flag === true){
                         flag = false;
                 }
-
                 //sets the base time to 1h before
-                if (s.type === 'W' && flag === false){
+                else if (s.type === 'W' && flag === false && lock === false){
                     d.base = s.slot_num - 2;
+                    lock = true;
                 }
             })
         })  
 
+        console.log(this.w.daysOfWeek[2].base + " base");
+
         //Sets the sleeping time to be 9 hours before the base and bleeds into previous day (unless day = 0)
         //does first one manunally to prevent oob error
-        if (this.w.daysOfWeek.base < RecommendedSleepNum){
-            for (var i = 0; i <= this.w.daysOfWeek.base; i ++)
-                this.daysOfWeek.time_slots[i].type = 'S';
+        if (this.w.daysOfWeek[0].base < RecommendedSleepNum){
+            for (var i = 0; i < this.w.daysOfWeek[0].base; i ++)
+                this.w.daysOfWeek[0].time_slots[i].type = 'S';
         } else {
-            for (var i = this.w.daysOfWeek.base - RecommendedSleepNum; i <= this.w.daysOfWeek.base; i ++)
-                this.daysOfWeek.time_slots[i].type = 'S';
+            for (var i = this.w.daysOfWeek[0].base - RecommendedSleepNum; i < this.w.daysOfWeek[0].base; i ++)
+                this.w.daysOfWeek[0].time_slots[i].type = 'S';
         }
 
         //bleeds into previous day if its too early
         
-        for (var index = 0; index < 7; index ++){
-            if (this.w.daysOfWeek.base < RecommendedSleepNum){
-                for (var i = 0; i <= this.w.daysOfWeek.base; i ++)
-                    this.daysOfWeek.time_slots[i].type = 'S';
+        for (var index = 1; index < 7; index ++){
+            if (this.w.daysOfWeek[index].base < RecommendedSleepNum){
+                for (var i = 0; i < this.w.daysOfWeek[index].base; i ++)
+                    this.w.daysOfWeek[index].time_slots[i].type = 'S';
                 //bleeds into day before
-                var remainder = RecommendedSleepNum - this.w.daysOfWeek.base;
-                for (var i = 47 - (RecommendedSleepNum - this.w.daysOfWeek.base); i <= 47; i ++)
-                    this.w.daysOfWeek[index -1].time_slots[i].type = 'S';
+                for (var i = 47 - (RecommendedSleepNum - this.w.daysOfWeek[index].base); i <= 47; i ++)
+                    this.w.daysOfWeek[index - 1].time_slots[i].type = 'S';
             } else {
-                for (var i = this.w.daysOfWeek.base - RecommendedSleepNum; i <= this.w.daysOfWeek.base; i ++)
-                    this.daysOfWeek.time_slots[i].type = 'S';
+                for (var i = this.w.daysOfWeek[index].base - RecommendedSleepNum; i < this.w.daysOfWeek[index].base; i ++)
+                    this.w.daysOfWeek[index].time_slots[i].type = 'S';
             }
             
         }
+
 
     }
 }
@@ -70,18 +71,18 @@ class Schedule{
 //start end time with food gap
 //auto insert a food gap
 var ts = [];
-ts.push(new time_slot(2,20,'W'));
-ts.push(new time_slot(2,21,'W'));
-ts.push(new time_slot(2,22,'W'));
-ts.push(new time_slot(2,23,'W'));
-ts.push(new time_slot(2,24,'W'));
-ts.push(new time_slot(2,25,'W'));
-ts.push(new time_slot(2,26,'W'));
-ts.push(new time_slot(2,27,'W'));
-ts.push(new time_slot(2,28,'W'));
-ts.push(new time_slot(2,29,'W'));
 
-var s = new Schedule(st);
+ts.push(new Time_Slot(2,21,'W'));
+ts.push(new Time_Slot(2,22,'W'));
+ts.push(new Time_Slot(2,23,'W'));
+ts.push(new Time_Slot(2,24,'W'));
+ts.push(new Time_Slot(2,25,'W'));
+ts.push(new Time_Slot(2,26,'W'));
+ts.push(new Time_Slot(2,27,'W'));
+ts.push(new Time_Slot(2,28,'W'));
+ts.push(new Time_Slot(2,29,'W'));
+
+var s = new Schedule(ts);
 for (var i = 0; i < 48; i ++){
-    console.log(s.week.daysOfWeek[2].time_slot[i].type);
+    console.log(i + " " + s.w.daysOfWeek[2].time_slots[i].type);
 }
